d5bd6478e6075c9c0e4d9a7b34cd91e2730d22d5
Yeshi Wangdi <ywblackberry@gmail.com>
2018-07-31 18:01:15 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Home from './components/Home';
+
+const RootNavigator = StackNavigator({
+  Main: {
+    screen: Home,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
+  }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+       <Home />
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
---
+++ components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+const Home = () => (
+    <View style={styles.container}>
+        <Text>Check out my Home Screen!</Text>
+
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignments: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default Home;
